30bba7cc7ae9d08537e7fafd6941a3b6ad8ee685
Mariana <luzmarianacarlo1999@gmail.com>
2020-01-29 18:09:31 -0400

    imagen ajustada para docentes y estudiantes

--- contenidosDocentes.php
+++ contenidosDocentes.php
@@ -57,39 +57,7 @@
       
   ?>
   
-    <div class="container">
-      <div class="text-center">
-      <div id="mycarousel" class="carousel slide" data-ride="carousel" data-interval="3000">
-            <ol class="carousel-indicators">
-              <li data-target="#mycarousel" data-slide-to="0" class="active"></li>
-              <li data-target="#mycarousel" data-slide-to="1"></li>
-              <li data-target="#mycarousel" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="img/title.png">
-                <div class="carousel-caption"></div>
-              </div>
-              <div class="carousel-item"> 
-                <img src="img/slide2Est.jpg"> 
-                <div class="carousel-caption"></div>
-              </div>
-              <div class="carousel-item"> 
-                <img src="img/slide3Est.png"> 
-                <div class="carousel-caption"></div>
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#mycarousel" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#mycarousel" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
-      </div>
-    </div>
+  <img src="img/tituloAdmi.png" alt="title">
 
     <div class="pos-f-t">  <!------COMIENZA POST-F-T------->  
       <div class="vertical-tab"> <!------COMIENZA VERTICAL-TAB------->  
